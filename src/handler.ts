9abad9042db497f81850d2accd7624b22cf8b5a5
Sergio Xalambrí <hello@sergiodxa.com>
2023-07-20 12:50:39 -0500

    Create handler independent of Cloudflare

---
+++ src/handler.ts
@@ -0,0 +1,37 @@
+import type { AppLoadContext, ServerBuild } from "@remix-run/server-runtime";
+import type { Context, MiddlewareHandler, Env, Input } from "hono";
+
+import { createRequestHandler } from "@remix-run/server-runtime";
+
+export interface RemixMiddlewareOptions<
+	E extends Env = Record<string, never>,
+	P extends string = "",
+	I extends Input = Record<string, never>,
+> {
+	build: ServerBuild;
+	mode?: "development" | "production";
+	getLoadContext?(
+		event: Context<E, P, I>,
+	): Promise<AppLoadContext> | AppLoadContext;
+}
+
+export function remix<
+	E extends Env = Record<string, never>,
+	P extends string = "",
+	I extends Input = Record<string, never>,
+>({
+	mode,
+	build,
+	getLoadContext = (context) => context.env as unknown as AppLoadContext,
+}: RemixMiddlewareOptions<E, P, I>): MiddlewareHandler {
+	return async function middleware(context) {
+		let requestHandler = createRequestHandler(build, mode);
+		let loadContext = getLoadContext(context);
+		return await requestHandler(
+			context.req.raw,
+			loadContext instanceof Promise ? await loadContext : loadContext,
+		);
+	};
+}
+
+export { createRequestHandler } from "@remix-run/server-runtime";
