20e75d09acfbaded1846b1a770fa25c99e128ce3
rphlmr <raphael@uaerom.com>
2024-01-17 19:05:44 +0100

    rework httpsOnly and add tests

--- src/security.ts
+++ src/security.ts
@@ -1,13 +1,19 @@
 import { createMiddleware } from "hono/factory";
 
+const https = "https:";
+
 /**
  * Enforce HTTPS connections.
  */
 export function httpsOnly() {
 	return createMiddleware(async (c, next) => {
 		let url = new URL(c.req.url);
-		if (url.protocol !== "http:") return await next();
-		url.protocol = "https:";
+
+		if (url.protocol === https) {
+			return await next();
+		}
+
+		url.protocol = https;
 		c.redirect(url.toString());
 	});
 }
