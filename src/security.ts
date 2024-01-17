ad53e7ddb431f822756ed0ca82a2685f5bd39bf5
rphlmr <raphael@uaerom.com>
2024-01-17 16:38:47 +0100

    update middlewares to follow hono convention

--- src/security.ts
+++ src/security.ts
@@ -1,13 +1,13 @@
-import type { MiddlewareHandler } from "hono";
+import { createMiddleware } from "hono/factory";
 
 /**
  * Enforce HTTPS connections.
  */
-export function httpsOnly(): MiddlewareHandler {
-	return async function middleware(context, next) {
-		let url = new URL(context.req.url);
+export function httpsOnly() {
+	return createMiddleware(async (c, next) => {
+		let url = new URL(c.req.url);
 		if (url.protocol !== "http:") return await next();
 		url.protocol = "https:";
-		context.redirect(url.toString());
-	};
+		c.redirect(url.toString());
+	});
 }
